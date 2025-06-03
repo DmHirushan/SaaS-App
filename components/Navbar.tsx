a92b1c32b0c302f79735982f5f4b0b6d741a5d1b
DmHirushan <dimuthsamaranayaka756@gmail.com>
2025-06-03 19:49:43 +0530

    auth implemented

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import NavItems from "@/components/NavItems";
+import {SignedIn, SignedOut, SignInButton, UserButton} from "@clerk/nextjs";
 
 const Navbar = () => {
     return (
@@ -17,8 +18,15 @@ const Navbar = () => {
                 </div>
             </Link>
             <div className='flex items-center gap-8'>
-                <NavItems/>
-                <p>Sign In</p>
+                <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className='btn-signin'>Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
             </div>
         </nav>
     )
